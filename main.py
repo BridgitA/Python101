ab803a8748972d5cd9f99e8030a49c8d6e31ab8a
Bridgit Arika <bridgitmob@gmail.com>
2020-09-24 00:33:15 -0500

    input for python

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("   /|")
-print("  / | ")
-print(" /  |")
-print("/___|")
+
+print('Pick a number: ')
+num1 = input()
+print('Pick another number ')
+num2 = input()
+
+checksum = int(num1) + int(num2)
+print(checksum)
