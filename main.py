71ebf36ce059a095db26962b0d3bad6fd448cbef
Bridgit Arika <bridgitmob@gmail.com>
2020-09-25 00:08:13 -0500

    working with lists in python

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-
-print('Pick a number: ')
-num1 = input()
-print('Pick another number ')
-num2 = input()
-
-checksum = int(num1) + int(num2)
-print(checksum)
+fruits = ['mango', 'pear', 'banana']
+print(fruits[1])
